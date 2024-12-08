7488f436a412ad4732d0e725a59a9255fce589ea
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2024-12-08 17:54:25 +0000

    did lessons 8 and 9

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,24 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT * FROM CUSTOMERS
+WHERE LastName = 'Stevensen' OR LastName = 'Stevenson' 
+OR LastName = 'Stephenson' OR LastName = 'Stuyvesant';
+
+--there was another way of doing this. wowzers.
+SELECT * FROM Reservations
+JOIN Customers ON Reservations.CustomerID = Customers.CustomerID
+WHERE (CUSTOMERS.LastName = 'Stevensen' OR CUSTOMERS.LastName = 'Stevenson' 
+OR CUSTOMERS.LastName = 'Stephenson' OR CUSTOMERS.LastName = 'Stuyvesant') 
+AND (RESERVATIONS.PartySize = 4)
+AND (RESERVATIONS.Date > '2022-06-14');
+
+--very different but better
+SELECT Customers.FirstName, Customers.LastName, Reservations.Date, Reservations.PartySize
+FROM Reservations
+JOIN Customers ON Customers.CustomerID = Reservations.CustomerID
+WHERE Customers.LastName LIKE 'St%'
+AND PartySize = 4
+ORDER BY Reservations.Date DESC;
+
--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,22 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+SELECT * FROM Customers
+WHERE Email = 'smac@kinetecoinc.com';
+
+--doesn't exist will add them
+INSERT INTO Customers (FirstName, LastName, Email)
+VALUES ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+
+SELECT * FROM Reservations;
+
+SELECT CustomerID FROM Customers WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Reservations(CustomerID, Date, PartySize)
+VALUES ( (SELECT CustomerID FROM Customers WHERE Email = 'smac@kinetecoinc.com') , '2022-08-12 18:00', 5);
+
+SELECT * FROM Reservations
+WHERE Date = '2022-08-12 18:00';
+--lets see the answer now
+
